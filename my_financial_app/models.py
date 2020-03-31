4b32ee3c171ecf9b5fcf29a0ff2a7b5779aaf155
yari <yari.taft@gmail.com>
2020-03-31 15:12:11 -0300

    Todo changes.

--- my_financial_app/models.py
+++ my_financial_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True)
+    created_date = models.DateTimeField(default=now)
 
     def __str__(self):
         """A string representation of the model."""
